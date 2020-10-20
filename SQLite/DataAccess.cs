74baf62f30e36154427d91c209df3fe5c95ec681
Marnus Tainthong <marnusfrank@gmail.com>
2020-10-20 20:14:17 +0700

    Add project files.

---
+++ SQLite/DataAccess.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLite
+{
+    class DataAccess
+    {
+        public static void InitializeDatabase()
+        {
+            //await ApplicationData.Current.LocalFolder.CreateFileAsync("sqliteSample.db", CreationCollisionOption.OpenIfExists);
+            //string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "sqliteSample.db");
+            using (SqliteConnection db =
+               new SqliteConnection("Filename=sqliteSample.db"))
+            {
+                db.Open();
+
+                String tableCommand = "CREATE TABLE IF NOT EXISTS " +
+                    "Person (Id INTEGER PRIMARY KEY, " +
+                    "Fname NVARCHAR(30) NULL," +
+                    "Lname NVARXHAR(30) NULL)";
+
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+
+                createTable.ExecuteReader();
+            }
+        }
+
+        public static void AddData(string Fname, string Lname)
+        {
+            using (SqliteConnection db =
+              new SqliteConnection("Filename=sqliteSample.db"))
+            {
+                db.Open();
+
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+
+                // Use parameterized query to prevent SQL injection attacks
+                insertCommand.CommandText = "INSERT INTO Person VALUES (NULL, @firstName, @lastName);";
+                insertCommand.Parameters.AddWithValue("@firstName", Fname);
+                insertCommand.Parameters.AddWithValue("@lastName", Lname);
+
+                insertCommand.ExecuteReader();
+
+                db.Close();
+            }
+
+        }
+
+        public static List<String> GetData()
+        {
+            List<String> entries = new List<string>();
+
+            using (SqliteConnection db =
+               new SqliteConnection("Filename=sqliteSample.db"))
+            {
+                db.Open();
+
+                SqliteCommand selectCommand = new SqliteCommand
+                    ("SELECT * from Person", db);
+
+                SqliteDataReader query = selectCommand.ExecuteReader();
+
+                while (query.Read())
+                {
+                    entries.Add("(id = "+query.GetString(0)+ ") Name = "+ query.GetString(1)+ " Lastname = "+ query.GetString(2));
+                }
+
+                db.Close();
+            }
+
+            return entries;
+        }
+    }
+}
---
+++ SQLite/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SQLite
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataAccess.InitializeDatabase();
+        }
+
+        private void btn_submit_Click(object sender, RoutedEventArgs e)
+        {
+            DataAccess.AddData(tbFname.Text, tbLname.Text);
+            string output = "";
+            foreach(string data in DataAccess.GetData())
+            {
+                output += data + "\n";
+            }
+            MessageBox.Show(output);
+        }
+    }
+}
